0e55721d055986f4df1da3ff8890d0729b08ffd1
Tomasz Kramkowski <tk@the-tk.com>
2015-06-13 21:02:21 +0100

    mail.py: Template env now uses feedparser defined elements
    The accessible list of fields is now the same as the list provided as
    feedparser. This might be why pubDate was never set. In any case, this
    gives much more options.
    
    Additionally, a set of functions accessible from within the the
    templates now exists, currently the only accessible function is
    time.strftime.

--- mail.py
+++ mail.py
@@ -19,57 +19,91 @@
 import os
 from email.mime.text import MIMEText
 from jinja2 import FileSystemLoader, Environment
+import time
 
-channel_atts = [
-        'title',
-        'link',
-        'description',
-        'language',
-        'copyright',
-        'managingEditor',
-        'webMaster',
-        'pubDate',
-        'lastBuildDate',
-        'category',
-        'generator',
-        'docs',
-        'cloud',
-        'ttl',
-        'image',
-        'image',
-        'rating',
-        'textInput',
-        'skipHours',
-        'skipDays'
-        ]
-
-item_attrs = [
-        'title',
-        'link',
-        'description',
+feed_attrs = [
         'author',
-        'category',
+        'author_detail',
+        'cloud',
+        'contributors',
+        'docs',
+        'errorreportsto',
+        'generator',
+        'generator_detail',
+        'icon',
+        'id',
+        'image',
+        'info',
+        'info_detail',
+        'language',
+        'license',
+        'link',
+        'links',
+        'logo',
+        'published',
+        'published_parsed',
+        'publisher',
+        'publisher_detail',
+        'rights',
+        'rights_detail',
+        'subtitle',
+        'subtitle_detail',
+        'tags',
+        'textinput',
+        'title',
+        'title_detail',
+        'ttl',
+        'updated',
+        'updated_parsed'
+]
+
+entry_attrs = [
+        'author',
+        'author_detail',
         'comments',
-        'enclosure',
-        'guid',
-        'pubDate',
-        'source'
-        ]
+        'content',
+        'contributors',
+        'created',
+        'created_parsed',
+        'enclosures',
+        'expired',
+        'expired_parsed',
+        'id',
+        'license',
+        'link',
+        'links',
+        'published',
+        'published_parsed',
+        'publisher',
+        'publisher_detail',
+        'source',
+        'summary',
+        'summary_detail',
+        'tags',
+        'title',
+        'title_detail',
+        'updated',
+        'updated_parsed'
+]
+
+tfuncs = {
+        'strftime': time.strftime
+}
 
 def gen_channel_tenv(channel):
     tenv = dict()
-    for attr in channel_atts:
+    for attr in feed_attrs:
         tenv[attr] = channel.get(attr, None)
     return tenv
 
 def gen_item_tenv(item):
     tenv = dict()
-    for attr in item_attrs:
+    for attr in entry_attrs:
         tenv[attr] = item.get(attr, None)
     return tenv
 
 def get_tenv(channel, item):
-    return {'channel': channel, 'item': item}
+    return {'channel': channel, 'item': item, 'func': tfuncs}
 
 def render_content(channel_tenv, item_tenv, template):
     template_name = os.path.basename(template)
